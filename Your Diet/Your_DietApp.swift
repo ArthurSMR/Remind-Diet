e487d6fddacc8211a1833ac4b79010dbf7e7bb50
ArthurSMR <arthursmr17@gmail.com>
2023-07-01 22:10:28 -0300

    First Setup

---
+++ Your Diet/Your_DietApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_DietApp.swift
+//  Your Diet
+//
+//  Created by Arthur Santos Machado Rodrigues on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Your_DietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
